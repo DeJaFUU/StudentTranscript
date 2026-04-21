1f33922b156c7c1e9ca0a48140bffa4aec099831
Frank <frankwoord@live.nl>
2026-04-21 20:17:55 +0200

    Finalized method designs

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,29 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    //TODO: Design specification for this method
-    // REQUIRES / MODIFIES / EFFECTS
+    // REQ course with grade (between 1.0 and 4.0)
+    // MOD: List of Grades
+    // Effect adds a course with grade to the list of grades from a student
     public void addGrade(String course, double grade) {
     }
 
-    //TODO: Design specification for this method
-    // This method should return course name and grade in some consistent String format
+    // REQ Course name
+    // MOD nothing
+    // Effect return course name if it exists else false
     public String getCourseAndGrade(String course) {
         return null;
     }
 
-    //TODO: Design specification for this method
-    //
+    // REQ Nothing
+    // MOD nothing
+    // effect prints course names with grades earned
     public void printTranscript() {
     }
 
     //TODO: Design specification for this method
-    //
+    // REQ CourseName
+    // MOD nothing
+    // effect returns course name and grade in format CourseName:Grade
     public double getGPA() {
         return 0.0;
     }
